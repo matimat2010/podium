21709a54a9a097d500f976c3202bea4ff7860bf1
Matt Alvarado <matt.alvarado@lee.net>
2023-07-21 10:33:39 -0500

    Initial commit

---
+++ server/server.js
@@ -0,0 +1,79 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const vendors = [];
+const app = express();
+app.use(bodyParser.json());
+
+app.post("/webhook", (req, res) => {
+    console.log(req.body); // Logs the received JSON data
+    res.sendStatus(200); // Sends a success response to the client
+});
+
+app.post("/loadvendors", (req, res) => {
+    const vendorData = req.body;
+
+    if (Array.isArray(vendorData)) {
+        // If the request body is an array, add each object to the "vendors" array
+        vendorData.forEach((vendorObj) => {
+            vendors.push(vendorObj);
+            console.log("Vendor data added:", vendorObj);
+        });
+    } else if (typeof vendorData === "object") {
+        // If the request body is a single object, add it to the "vendors" array
+        vendors.push(vendorData);
+        console.log("Vendor data added:", vendorData);
+    } else {
+        return res.status(400).send("Invalid JSON format.");
+    }
+
+    res.sendStatus(200); // Sends a success response to the client
+});
+
+app.put("/updatevendor/:code", (req, res) => {
+    const codeToUpdate = req.params.code;
+    const updatedVendor = req.body;
+
+    // Find the index of the vendor with the matching code
+    const indexToUpdate = vendors.findIndex(
+        (vendor) => vendor.Code === codeToUpdate
+    );
+
+    if (indexToUpdate === -1) {
+        // If the code is not found in the array, respond with 404 Not Found
+        return res.status(404).send("Vendor not found.");
+    }
+
+    // Update the vendor in the "vendors" array using the index
+    vendors[indexToUpdate] = updatedVendor;
+    console.log("Vendor updated:", updatedVendor);
+
+    res.sendStatus(200); // Sends a success response to the client
+});
+
+app.delete("/deletevendor/:code", (req, res) => {
+    const codeToDelete = req.params.code;
+
+    // Find the index of the vendor with the matching code
+    const indexToDelete = vendors.findIndex(
+        (vendor) => vendor.Code === codeToDelete
+    );
+
+    if (indexToDelete === -1) {
+        // If the code is not found in the array, respond with 404 Not Found
+        return res.status(404).send("Vendor not found.");
+    }
+
+    // Remove the vendor from the "vendors" array using the index
+    const deletedVendor = vendors.splice(indexToDelete, 1)[0];
+    console.log("Vendor deleted:", deletedVendor);
+
+    res.sendStatus(200); // Sends a success response to the client
+});
+
+app.get("/printvendors", (req, res) => {
+    res.json(vendors); // Respond with the "vendors" array in JSON format
+});
+
+app.listen(3000, () => {
+    console.log("Server is running on http://localhost:3000");
+});
